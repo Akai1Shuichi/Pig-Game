544e0117070f18d8318f1350e4b0b24c7af6adba
KTrong <toancccccccc@gmail.com>
2022-11-23 19:59:31 +0700

    7h59

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ const init = () => {
   playing = true;
 
   player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--active');
-  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
   player1El.classList.remove('player--active');
 };
 
